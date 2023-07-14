aeedecb758d661513a9999072c867ee8af23650c
bioroid17 <trace1997@ajou.ac.kr>
2023-07-14 17:35:51 +0900

    mysite02 - board

---
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/BoardController.java
@@ -0,0 +1,33 @@
+package com.bitacademy.mysite.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BoardController
+ */
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		
+		String actionName = request.getParameter("a");
+		if("writeform".equals(actionName)) {
+			
+		} else if("write".equals(actionName)) {
+			
+		} else {
+			/* default action */
+			request.getRequestDispatcher("/WEB-INF/views/board/list.jsp").forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
