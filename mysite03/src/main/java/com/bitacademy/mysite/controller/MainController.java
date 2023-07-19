34e5a25d27b9ef10640d76a54817053beee11ca0
bioroid17 <trace1997@ajou.ac.kr>
2023-07-19 11:22:57 +0900

    mysite03 - init again

--- mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.bitacademy.mysite03.controller;
+package com.bitacademy.mysite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
@@ -0,0 +1,236 @@
+package com.bitacademy.mysite.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Repository;
+
+import com.bitacademy.mysite.vo.UserVo;
+
+@Repository
+public class UserRepository {
+
+	public Boolean insert(UserVo vo) {
+		boolean result = false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnection();
+			String sql = "insert into user values (null, ?, ?, password(?), ?, now())";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPassword());
+			pstmt.setString(4, vo.getGender());
+
+			int count = pstmt.executeUpdate();
+			
+			result = count == 1;
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	public void update(UserVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnection();
+			if ("".equals(vo.getPassword())) {
+				String sql = "update user set name=?, gender=? where no=?";
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setString(1, vo.getName());
+				pstmt.setString(2, vo.getGender());
+				pstmt.setLong(3, vo.getNo());
+			} else {
+				String sql = "update user set name=?, password=password(?), gender=? where no=?";
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setString(1, vo.getName());
+				pstmt.setString(2, vo.getPassword());
+				pstmt.setString(3, vo.getGender());
+				pstmt.setLong(4, vo.getNo());
+			}
+
+			pstmt.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public UserVo findByEmailAndPassword(String email, String password) {
+		UserVo authUser = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			String sql = "select no, name from user where email=? and password=password(?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, email);
+			pstmt.setString(2, password);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				authUser = new UserVo();
+				authUser.setNo(rs.getLong(1));
+				authUser.setName(rs.getString(2));
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if( rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return authUser;
+	}
+	
+	public UserVo findByNo(Long no) {
+		UserVo userVo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			String sql = "select no, name, email, gender from user where no=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, no.toString());
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				userVo = new UserVo();
+				userVo.setNo(rs.getLong(1));
+				userVo.setName(rs.getString(2));
+				userVo.setEmail(rs.getString(3));
+				userVo.setGender(rs.getString(4));
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if( rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return userVo;
+	}
+	
+	public Boolean checkPassword(String no, String password) {
+		System.out.println(no);
+		boolean equal = false;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			String sql = "select password(?)=(select password from user where no=?) from user where no=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, password);
+			pstmt.setString(2, no);
+			pstmt.setString(3, no);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getInt(1) == 1) {
+					equal = true;
+				}
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if( rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return equal;
+	}
+
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+
+		Class.forName("org.mariadb.jdbc.Driver");
+
+		String url = "jdbc:mariadb://192.168.0.162:3306/webdb?charset=utf8";
+		conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		return conn;
+	}
+}
