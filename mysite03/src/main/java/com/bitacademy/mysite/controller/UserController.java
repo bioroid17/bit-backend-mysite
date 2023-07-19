699f8111503726a0bdd4e54295a9fb900afe37ab
bioroid17 <trace1997@ajou.ac.kr>
2023-07-19 11:56:23 +0900

    mysite03 - user

--- mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
@@ -1,8 +1,36 @@
 package com.bitacademy.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bitacademy.mysite.service.UserService;
+import com.bitacademy.mysite.vo.UserVo;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		// return 값은 ViewResolver로 간다.
+		return "user/join";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		userService.addUser(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping("/joinsuccess")
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
 
 }
--- mysite03/src/main/java/com/bitacademy/mysite/vo/UserVo.java
+++ mysite03/src/main/java/com/bitacademy/mysite/vo/UserVo.java
@@ -43,5 +43,10 @@ public class UserVo {
 	public void setJoinDate(String joinDate) {
 		this.joinDate = joinDate;
 	}
+	@Override
+	public String toString() {
+		return "UserVo [no=" + no + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
+				+ gender + ", joinDate=" + joinDate + "]";
+	}
 	
 }
