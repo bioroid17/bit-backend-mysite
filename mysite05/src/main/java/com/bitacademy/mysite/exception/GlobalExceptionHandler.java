1b27e827f887583f3dab2e1eca7c54a9b33bc473
bioroid17 <trace1997@ajou.ac.kr>
2023-07-26 11:37:15 +0900

    mysite05 - 404 예외 처리

--- mysite05/src/main/java/com/bitacademy/mysite/exception/GlobalExceptionHandler.java
+++ mysite05/src/main/java/com/bitacademy/mysite/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 // Container가 이 어노테이션을 알아본다.
 @ControllerAdvice
@@ -17,6 +18,11 @@ public class GlobalExceptionHandler {
 	// Exception.class: 모든 Exception을 받을 것
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Model model, Exception ex) {
+		// 1. 404 Error 처리
+		if(ex instanceof NoHandlerFoundException) {
+			return "error/404";
+		}
+		
 		// 1. 로깅
 		StringWriter errors = new StringWriter();
 		ex.printStackTrace(new PrintWriter(errors));	// OutputStream을 매개변수로 줄 수 있다.
